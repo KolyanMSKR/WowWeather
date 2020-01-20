5dab2ab2dbabd38be143686e38b9728d51aa8ed9
asd dsa <asd@192.168.161.128>
2020-01-20 01:15:04 -0800

    Configured particles effects

--- WowWeather/Animation Views/RainView.swift
+++ WowWeather/Animation Views/RainView.swift
@@ -1,15 +1,15 @@
 //
-//  SnowView.swift
+//  RainView.swift
 //  WowWeather
 //
-//  Created by asd dsa on 1/10/20.
+//  Created by asd dsa on 1/14/20.
 //  Copyright © 2020 asd dsa. All rights reserved.
 //
 
 import UIKit
 import QuartzCore
 
-class SnowView: UIView {
+class RainView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -20,22 +20,22 @@ class SnowView: UIView {
         emitter.emitterShape = .rectangle
         
         let emitterCell = CAEmitterCell()
-        emitterCell.contents = UIImage(named: "snowflake.png")!.cgImage
-        emitterCell.birthRate = 50
+        emitterCell.contents = UIImage(named: "raindrop.png")!.cgImage
+        emitterCell.birthRate = 500
         emitterCell.lifetime = 5.5
         emitterCell.color = UIColor.white.cgColor
         emitterCell.redRange = 0.0
         emitterCell.blueRange = 0.1
         emitterCell.greenRange = 0.0
-        emitterCell.velocity = 10
-        emitterCell.velocityRange = 300
+        emitterCell.velocity = 1
+        emitterCell.velocityRange = 30
         emitterCell.emissionRange = CGFloat(Double.pi/2)
         emitterCell.emissionLongitude = CGFloat(-Double.pi)
         emitterCell.yAcceleration = 70
-        emitterCell.xAcceleration = 0
-        emitterCell.scale = 0.33
-        emitterCell.scaleRange = 1.25
-        emitterCell.scaleSpeed = -0.1
+        emitterCell.xAcceleration = 15
+        emitterCell.scale = 0.01
+        emitterCell.scaleRange = 0.00
+        emitterCell.scaleSpeed = 0.04
         emitterCell.alphaRange = 0.5
         emitterCell.alphaSpeed = -0.15
         
